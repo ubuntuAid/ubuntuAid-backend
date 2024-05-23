45697526074c2d74ac5a85f4c77eb18baea6225c
Masambu Emmanuel <masambue11@gmail.com>
2024-05-23 17:05:49 +0300

    Modify the middlewares.js file

--- config/middlewares.js
+++ config/middlewares.js
@@ -10,3 +10,21 @@ module.exports = [
   'strapi::favicon',
   'strapi::public',
 ];
+module.exports = [
+  // ...
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  // ...
+];
